cf845ff038d3fb7ec12ae2a2276cf60533172cb7
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:23:04 +0100

    Cleanup tests

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-#This file is part electronic_mail_wizard module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part electronic_mail_wizard module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.pool import Pool
 from .electronic_mail_wizard import *
 from .template import *
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
-#This file is part electronic_mail_wizard module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part electronic_mail_wizard module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from .test_electronic_mail_wizard import suite
 
