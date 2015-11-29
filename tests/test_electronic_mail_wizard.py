7cca1831b9c4945ee0d9fa13ff1b5271bc635ebb
resteve <resteve@zikzakmedia.com>
2015-11-29 22:21:06 +0100

    Tests

--- tests/test_electronic_mail_wizard.py
+++ tests/test_electronic_mail_wizard.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part electronic_mail_wizard module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the electronic_mail_wizard module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ElectronicMailWizardTestCase(unittest.TestCase):
-    'Test Helloword module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('electronic_mail_wizard')
-
-    def test0005views(self):
-        'Test views'
-        test_view('electronic_mail_wizard')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class ElectronicMailWizardTestCase(ModuleTestCase):
+    'Test Electronic Mail Wizard module'
+    module = 'electronic_mail_wizard'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         ElectronicMailWizardTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
